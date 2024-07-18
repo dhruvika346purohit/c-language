9a1dbc87ee226337ef8c604fb6df96667101302f
dhruvika346purohit <purohitdhruvika87@gmail.com>
2024-07-18 18:14:04 +0530

    Add files via upload

---
+++ basic information.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("name:purohit dhruvika sureshbhai\n");
+	printf("email: purohitdhruvika87@gmail.com\n");
+	printf("age: 17\n");
+	printf("phone no.:9978629429\n");
+	printf("qualification:12th pass\n");
+	printf("college: SDJ\n");
+	printf("address:c-704,abhinav heights,nr.s.m.c garden,dindoli,surat.\n");
+	
+}
