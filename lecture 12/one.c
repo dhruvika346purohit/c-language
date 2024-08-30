5ac08c1e61b7d07055c930b19a4d0c0ad42f1ad5
purohit dhruvika <purohitdhruvika87@gmail.com>
2024-08-30 18:53:23 +0530

    Add files via upload

---
+++ lecture 12/one.c
@@ -0,0 +1,14 @@
+//1 to 10
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=1;
+	while(a<=10)
+	{
+		printf("%d ",a);
+		a++;
+	}
+	
+}
+
---
+++ lecture 12/three.c
@@ -0,0 +1,14 @@
+//n to 1
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i=1,n;
+	printf("enter the value of n=");
+	scanf("%d",&n);
+	while(n>=i)
+	{
+		printf("%d ",n);
+		n--;
+	}
+}
